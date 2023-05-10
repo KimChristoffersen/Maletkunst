06b6396c79199e4a41521dbdd8ab79761723a9bc
Kim <10434639@ucn.dk>
2023-05-10 10:42:04 +0200

    Orderline removed and start of concurency

--- Maletkunst.DAL/SQL/OrdersSqlDao.cs
+++ Maletkunst.DAL/SQL/OrdersSqlDao.cs
@@ -8,7 +8,7 @@ public class OrdersSqlDao : IOrdersDataAccess
 {
     private const string connectionString = @"Data Source=hildur.ucn.dk; Initial Catalog=DMA-CSD-V221_10434660; User ID=DMA-CSD-V221_10434660; Password=Password1!;";
 
-    private IOrderLineMvcDataAccess _orderLineMvcDataAccess = new OrderLineMvcSqlDao();
+    private IPaintingsDataAccess _paintingSqlDataAccess = new PaintingsSqlDataAccess();
 
     public IEnumerable<Order> GetAllOrders()
     {
@@ -35,44 +35,60 @@ public class OrdersSqlDao : IOrdersDataAccess
                 OrderDate = (DateTime)reader["OrderDate"],
                 Status = (string)reader["Status"],
                 Total = (decimal)reader["Total"],
-                OrderLines = _orderLineMvcDataAccess.GetAllOrderLinesByOrderNumber((int)reader["OrderNumber"])
+                OrderLines = GetAllOrderLinesByOrderNumber((int)reader["OrderNumber"])
             });
         }
         return orders;
-    }
-
-    public void Test()
-    {
-
     }
 
     public int CreateOrder(Order order)
     {
         string queryStringOrder = @"insert into [Order] (Status, Total) values(@Status, @Total); SELECT CAST(scope_identity() AS int)";
         string queryStringOrderLine = @"insert into [OrderLine] values(@Quantity, @SubTotal, @OrderNumber, @Painting_Id)";
+        string queryStringCorrectPaintingsStock = @"UPDATE Painting SET Stock = @stock WHERE Id = @Painting_Id AND Stock > 0";
+
         using SqlConnection connection = new SqlConnection(connectionString);
         connection.Open();
+
         SqlTransaction transaction = connection.BeginTransaction();
         SqlCommand commandOrder = new SqlCommand(queryStringOrder, connection, transaction);
         SqlCommand commandOrderLine = new SqlCommand(queryStringOrderLine, connection, transaction);
+        SqlCommand commandCorrectPaintingsStock = new SqlCommand(queryStringCorrectPaintingsStock, connection, transaction);
+
         commandOrder.Parameters.AddWithValue("@Status", order.Status);
         commandOrder.Parameters.AddWithValue("@Total", order.Total);
 
         try
         {
-
+            // Order generation
             int newGeneratedOrderNumber = (int)commandOrder.ExecuteScalar();
 
-            foreach (OrderLine orderLine in order.OrderLines)
+            // OrderLine generation
+            bool rowsAreAffected = true;
+            while (rowsAreAffected)
             {
-                commandOrderLine.Parameters.AddWithValue("@Quantity", orderLine.Quantity);
-                commandOrderLine.Parameters.AddWithValue("@SubTotal", orderLine.SubTotal);
-                commandOrderLine.Parameters.AddWithValue("@OrderNumber", newGeneratedOrderNumber);
-                commandOrderLine.Parameters.AddWithValue("@Painting_Id", orderLine.Painting.Id);
-                commandOrderLine.ExecuteNonQuery();
+                foreach (OrderLine orderLine in order.OrderLines)
+                {
+                    commandOrderLine.Parameters.AddWithValue("@Quantity", orderLine.Quantity);
+                    commandOrderLine.Parameters.AddWithValue("@SubTotal", orderLine.SubTotal);
+                    commandOrderLine.Parameters.AddWithValue("@OrderNumber", newGeneratedOrderNumber);
+                    commandOrderLine.Parameters.AddWithValue("@Painting_Id", orderLine.Painting.Id);
+                    commandOrderLine.ExecuteNonQuery();
+
+                    // PaintingsStock correction
+                    commandCorrectPaintingsStock.Parameters.AddWithValue("@Stock", 0);
+                    commandCorrectPaintingsStock.Parameters.AddWithValue("@Painting_Id", orderLine.Painting.Id);
+                    if (commandCorrectPaintingsStock.ExecuteNonQuery() == 0) { rowsAreAffected = false; }
+                }
             }
+
+
+
+
             transaction.Commit();
             return newGeneratedOrderNumber;
+
+
         }
 
         catch (Exception e)
@@ -88,4 +104,39 @@ public class OrdersSqlDao : IOrdersDataAccess
             }
         }
     }
+
+
+
+    public IEnumerable<OrderLine> GetAllOrderLinesByOrderNumber(int orderNumber)
+    {
+        string queryString = @"SELECT * FROM OrderLine WHERE Order_Id = @OrderNumber";
+        using SqlConnection connection = new SqlConnection(connectionString);
+        SqlCommand command = new SqlCommand(queryString, connection);
+        command.Parameters.AddWithValue("@OrderNumber", orderNumber);
+
+        connection.Open();
+
+        try { return BuildListOfOrderLines(command); }
+
+        catch (Exception ex) { throw new Exception("ERROR occurred while getting all order lines", ex); }
+    }
+
+
+    private IEnumerable<OrderLine> BuildListOfOrderLines(SqlCommand command)
+    {
+        SqlDataReader reader = command.ExecuteReader();
+        ICollection<OrderLine> orderLines = new List<OrderLine>();
+        while (reader.Read())
+        {
+            orderLines.Add(new OrderLine()
+            {
+                OrderLineId = (int)reader["OrderLineId"],
+                Quantity = (int)reader["Quantity"],
+                SubTotal = (decimal)reader["SubTotal"],
+                Painting = _paintingSqlDataAccess.GetPaintingById((int)reader["Painting_Id"])
+            });
+        }
+        return orderLines;
+    }
+
 }
