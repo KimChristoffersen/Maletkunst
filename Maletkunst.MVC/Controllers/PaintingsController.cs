96d4fb3f6e37a810127d7857bf2d10cdedce47e9
bobosandwich <102587853+bobosandwich@users.noreply.github.com>
2023-05-11 08:39:17 +0200

    Billede filtrering - view data

--- Maletkunst.MVC/Controllers/PaintingsController.cs
+++ Maletkunst.MVC/Controllers/PaintingsController.cs
@@ -6,39 +6,59 @@ namespace Maletkunst.MVC.Controllers;
 
 public class PaintingsController : Controller
 {
-    IPaintingsDataAccess _client;
+	IPaintingsDataAccess _client;
 
-    public PaintingsController(IPaintingsDataAccess client)
-    {
-        _client = client;
-    }
-    // GET: PaintingsController
-    public IActionResult Index(string searchString)
-    {
-        IEnumerable<Painting> paintings;
-        if (!string.IsNullOrEmpty(searchString)) { paintings = _client.GetAllPaintingsByFreeSearch(searchString); }
-        else { paintings = _client.GetAllAvailablePaintings(); }
+	public PaintingsController(IPaintingsDataAccess client)
+	{
+		_client = client;
+	}
+	// GET: PaintingsController
+	public IActionResult Index(string category, string searchString)
+	{
+		IEnumerable<Painting> paintings;
 
-        return View(paintings);
-    }
+		if (!string.IsNullOrEmpty(searchString))
+		{
+			if (!string.IsNullOrEmpty(category))
+			{
+				paintings = _client.GetAllPaintingsByFreeSearch(searchString).Where(p => p.Category == category);
+			}
+			else
+			{
+				paintings = _client.GetAllPaintingsByFreeSearch(searchString);
+			}
+		}
+		else if (!string.IsNullOrEmpty(category))
+		{
+			paintings = _client.GetAllPaintingsByCategory(category);
+		}
+		else
+		{
+			paintings = _client.GetAllAvailablePaintings();
+		}
 
-    public IActionResult GetPaintingsByCategory(string category)
-    {
-        var getPaintingsByCategory = _client.GetAllPaintingsByCategory(category);
-        return View("Index", getPaintingsByCategory);
-    }
+		ViewData["Category"] = category;
+		ViewData["SearchString"] = searchString;
+		return View(paintings);
+	}
 
-    public IActionResult Details(int id)
-    {
-        var painting = _client.GetPaintingById(id);
-        return View(painting);
-    }
 
-    public IActionResult GetPaintingById(int id)
-    {
+	public IActionResult GetPaintingsByCategory(string category)
+	{
+		var getPaintingsByCategory = _client.GetAllPaintingsByCategory(category);
+		return View("Index", getPaintingsByCategory);
+	}
 
-        var getPaintingById = _client.GetPaintingById(id);
-        return View("Index", getPaintingById);
-    }
+	public IActionResult Details(int id)
+	{
+		var painting = _client.GetPaintingById(id);
+		return View(painting);
+	}
+
+	public IActionResult GetPaintingById(int id)
+	{
+
+		var getPaintingById = _client.GetPaintingById(id);
+		return View("Index", getPaintingById);
+	}
 }
-
