95a44eb4f20ceb6c2faf100f05daa94a84f43f54
Kim <10434639@ucn.dk>
2023-05-16 12:12:10 +0200

    queries changed

--- Maletkunst.DAL/SQL/OrdersSqlDao.cs
+++ Maletkunst.DAL/SQL/OrdersSqlDao.cs
@@ -93,9 +93,10 @@ public class OrdersSqlDao : IOrdersDataAccess
 	public int CreateOrder(Order order)
 	{
 		// QUERIES DEFINITIONS FOR ORDER
-		//string queryStringOrder = @"insert into [Order] (Status, Total, Customer_Id) values(@Status, @Total, @Customer_Id); SELECT CAST(scope_identity() AS int)";
-		//string queryStringOrderLine = @"insert into [OrderLine] values(@Quantity, @SubTotal, @OrderNumber, @Painting_Id)";
-		//string queryStringCorrectPaintingsStock = @"UPDATE Painting SET Stock = @stock WHERE Id = @Painting_Id AND Stock > 0";
+		//string queryString_InsertOrder = @"insert into [Order] (Status, Total, Customer_Id) values(@Status, @Total, @Customer_Id); SELECT CAST(scope_identity() AS int)";
+		//string queryString_InsertOrderLine = @"insert into [OrderLine] values(@Quantity, @SubTotal, @OrderNumber, @Painting_Id)";
+		//string queryString_SelectPaintingsWithStock = @"SELECT * FROM Painting WHERE Id = @Painting_Id AND Stock > 0";
+		//string queryString_UpdatePaintingsStock = @"UPDATE Painting SET Stock = Stock - 1 WHERE Id = @Painting_Id";
 
 		// QUERIES DEFINITIONS FOR CUSTOMER
 		//string queryStringPerson = @"INSERT INTO Person (fName, lName, phone, email, personType) VALUES (@fName, @lName, @phone, @email, @personType); SELECT CAST(scope_identity() AS int)";
